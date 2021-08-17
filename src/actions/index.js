22915151d569dea9974c9396aee2d6314f7670a5
zhicong wen <zwen000@citymail.cuny.edu>
2021-08-16 23:01:13 -0400

    Redux-Thunk Done, section 260

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,15 @@
 // action creator
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-export const fetchPosts = async () => {
-    const response = await jsonPlaceHolder.post('/posts');
+export const fetchPosts = () => {
 
-    return {
-        type: "FETCH_POSTS",
-        payload: response
+    return async (dispatch, getState) => {
+        const response = await jsonPlaceHolder.post('/posts');
+
+        dispatch({ type:'FETCH_POSTS', payload:response })
+        // return {
+        //     type: "FETCH_POSTS",
+        //     payload: response
+        // };
     };
 };
