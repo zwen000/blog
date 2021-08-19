64dad3a0cc01f9600f6378a4066eaef727dceb64
zhicong wen <zwen000@citymail.cuny.edu>
2021-08-18 20:53:49 -0400

    section 272, fetching post list done

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,8 @@ import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 export const fetchPosts = () => {
 
     return async (dispatch, getState) => {
-        const response = await jsonPlaceHolder.post('/posts');
+        const response = await jsonPlaceHolder.get('/posts');
 
-        dispatch({ type:'FETCH_POSTS', payload:response })
-        // return {
-        //     type: "FETCH_POSTS",
-        //     payload: response
-        // };
+        dispatch({ type:'FETCH_POSTS', payload:response.data });
     };
 };
